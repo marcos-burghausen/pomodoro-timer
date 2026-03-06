bab792ca4542e92d317fd67155882857a97c4273
rafaelburghausen <rafaelburghausen@gmail.com>
2026-03-06 00:24:16 -0300

    feat: finix home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,42 +1,110 @@
+import { MaterialIcons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { AnimatedCircularProgress } from "react-native-circular-progress";
 import { TNavigationScreenProps } from "../AppRoutes";
 import { Theme } from "../shared/themes/Theme";
 
 export const Home = () => {
   const navigation = useNavigation<TNavigationScreenProps>();
   return (
-    <View style={{ backgroundColor: Theme.colors.backgground }}>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.primaryButton}>
-          <Text style={styles.primaryButtonText}>Iniciar</Text>
-        </TouchableOpacity>
-      </View>
+    <View style={styles.mainContainer}>
+      <TouchableOpacity
+        style={styles.settingsButton}
+        onPress={() => navigation.navigate("Settings")}
+      >
+        <MaterialIcons name="settings" size={28} color={Theme.colors.divider} />
+      </TouchableOpacity>
+      <View style={styles.container}>
+        <View style={styles.titleGroup}>
+          <View style={styles.titleContainer}>
+            <Text style={styles.titleText}>Pomodoro</Text>
+          </View>
 
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.primaryButton}>
-          <Text style={styles.primaryButtonText}>Pausar</Text>
-        </TouchableOpacity>
+          <View style={styles.stateContainer}>
+            <Text style={styles.stateText}>Hora de se concentrar</Text>
 
-        <TouchableOpacity style={styles.secondaryButton}>
-          <Text style={styles.secondaryButtonText}>Parar</Text>
-        </TouchableOpacity>
-      </View>
+            <Text style={styles.stateText}>Pausa curta</Text>
 
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.primaryButton}>
-          <Text style={styles.primaryButtonText}>Continuar</Text>
-        </TouchableOpacity>
+            <Text style={styles.stateText}>Pausa longa</Text>
 
-        <TouchableOpacity style={styles.secondaryButton}>
-          <Text style={styles.secondaryButtonText}>Reiniciar</Text>
-        </TouchableOpacity>
+            <Text style={styles.stateText}>Cronômetro em pausa</Text>
+          </View>
+
+          <View style={styles.progressContainer}>
+            <AnimatedCircularProgress
+              size={160}
+              width={7}
+              fill={10}
+              tintColor={Theme.colors.divider}
+              backgroundColor={Theme.colors.primary}
+              rotation={0}
+              children={() => <Text style={styles.progressText}>12:45</Text>}
+            />
+          </View>
+        </View>
+
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.primaryButton}>
+            <Text style={styles.primaryButtonText}>Iniciar</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.primaryButton}>
+            <Text style={styles.primaryButtonText}>Pausar</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.secondaryButton}>
+            <Text style={styles.secondaryButtonText}>Parar</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.primaryButton}>
+            <Text style={styles.primaryButtonText}>Continuar</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.secondaryButton}>
+            <Text style={styles.secondaryButtonText}>Reiniciar</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.pomodoroContainer}>
+          <Text style={styles.pomodoroText}>Pomodoros completos:</Text>
+
+          <View style={styles.pomodoroIndicatorComplete} />
+
+          <View style={styles.pomodoroIndicatorComplete} />
+
+          <View style={styles.pomodoroIndicatorComplete} />
+
+          <View style={styles.pomodoroIndicator} />
+        </View>
       </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 16,
+  },
+  container: {
+    gap: 36,
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  settingsButton: {
+    alignSelf: "flex-end",
+  },
+  titleGroup: {
+    gap: 24,
+  },
   primaryButton: {
     backgroundColor: Theme.colors.primary,
     paddingHorizontal: 24,
@@ -64,5 +132,51 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "center",
     gap: 16,
+  },
+  progressContainer: {
+    alignItems: "center",
+  },
+  progressText: {
+    color: Theme.colors.text,
+    fontFamily: Theme.fonts.interBold,
+    fontSize: Theme.fontSizes.title,
+  },
+  titleContainer: {
+    alignItems: "center",
+  },
+  titleText: {
+    color: Theme.colors.text,
+    fontFamily: Theme.fonts.interBold,
+    fontSize: Theme.fontSizes.title,
+  },
+  stateContainer: {
+    alignItems: "center",
+  },
+  stateText: {
+    color: Theme.colors.text,
+    fontFamily: Theme.fonts.interRegular,
+    fontSize: Theme.fontSizes.body,
+  },
+  pomodoroContainer: {
+    gap: 8,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  pomodoroText: {
+    color: Theme.colors.text,
+    fontFamily: Theme.fonts.interRegular,
+    fontSize: Theme.fontSizes.body,
+  },
+  pomodoroIndicator: {
+    width: 20,
+    height: 20,
+    borderRadius: "100%",
+    backgroundColor: Theme.colors.divider,
+  },
+  pomodoroIndicatorComplete: {
+    width: 20,
+    height: 20,
+    borderRadius: "100%",
+    backgroundColor: Theme.colors.primary,
   },
 });
